9058552f6d7c92187c5d89712b5b7b9e7e3d2492
Imisi Bailey <imisibailey@gmail.com>
2025-09-19 16:40:58 +0100

    Created a digital clock

---
+++ clock.js
@@ -0,0 +1,21 @@
+import  dayjs  from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
+
+
+function time() {
+  let now = dayjs()
+  const date = now.format('h:mm:ss a')
+  console.log(date)
+  document.querySelector('.js-clock').innerHTML = date;
+}
+time()
+setInterval(time, 1000);
+
+
+function date() {
+  let now = dayjs()
+  const date = now.format('dddd, D MMMM YYYY')
+  console.log(date)
+  document.querySelector('.js-date').innerHTML = date;
+}
+time()
+setInterval(date, 1000);
